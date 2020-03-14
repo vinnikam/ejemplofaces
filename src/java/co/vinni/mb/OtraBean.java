9bd4c3d197de042a1d9516f0d244067272f0fd39
Vinni <Sala 30@172.20.8.45>
2020-03-13 19:10:05 -0500

    Vinni- Mi pagina otra

---
+++ src/java/co/vinni/mb/OtraBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.vinni.mb;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Sala 30
+ */
+@Named(value = "otraBean")
+@Dependent
+public class OtraBean {
+
+    /**
+     * Creates a new instance of OtraBean
+     */
+    public OtraBean() {
+    }
+    
+}
